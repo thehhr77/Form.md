2e59f12f627d4217de0501147372d1846af99449
thehhr77 <thehhr77@users.noreply.github.com>
2026-09-02 15:53:13 +0330

    Status-bar inset fix (part 1): synchronous native insets in MainActivity (padding + keyboard handling, zeroed WebView insets), insetsHandling=disable, androidx.core dep; remove dead safe-area CSS (safe-top var, ::before filler, env() fallbacks)

--- android/app/src/main/java/com/thehhr/form/MainActivity.java
+++ android/app/src/main/java/com/thehhr/form/MainActivity.java
@@ -1,5 +1,29 @@
 package com.thehhr.form;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    View container = (View) bridge.getWebView().getParent();
+    container.setBackgroundColor(Color.BLACK);
+    ViewCompat.setOnApplyWindowInsetsListener(container, (v, insets) -> {
+      Insets bars = insets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
+      boolean keyboardVisible = insets.isVisible(WindowInsetsCompat.Type.ime());
+      Insets ime = insets.getInsets(WindowInsetsCompat.Type.ime());
+      v.setPadding(bars.left, bars.top, bars.right, keyboardVisible ? ime.bottom : bars.bottom);
+      return new WindowInsetsCompat.Builder(insets)
+          .setInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout(), Insets.of(0, 0, 0, 0))
+          .build();
+    });
+  }
+}
